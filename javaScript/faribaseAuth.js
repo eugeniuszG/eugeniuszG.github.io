af993b2bb4630d7b75ad6fdb7fb16161c18079b8
eugeneboroda <41344500+eugeneboroda@users.noreply.github.com>
2020-05-21 14:51:25 +0200

    done with Login project

--- javaScript/faribaseAuth.js
+++ javaScript/faribaseAuth.js
@@ -6,17 +6,22 @@ var country = document.querySelector('.row .country')
 var checkBox = document.getElementById('policies')
 
 
-var firstNameErr = emailErr = passwordErr =  countryErr = acceptionErr = true;
+var firstNameErr = lastNameErr = emailErr = passwordErr =  countryErr = acceptionErr = true;
 
 //submit button very important
 var btnSubmit = document.querySelector('.submit');
 
+//forgot pass button
+const forgotPass = document.querySelector('#reset-btn');
+
 const logout = document.querySelector('#logoutId');
 const account = document.querySelector('#accountId');
-const messageInit = document.querySelector('.lucida-font')
+const messageInit = document.querySelector('.lucida-font');
+const greetUser = document.querySelector('.greetUser');
 
 
-
+const modalAccInfo = document.querySelector('.modal-account');
+const accInfo = document.querySelector('.accountInfo')
 
 //display hint error
 function error(elemId, hintMsg){
@@ -36,6 +41,20 @@ function validator(){
         else{
             error('firstNameErr', '');
             firstNameErr = false;
+        }
+    }
+    //validate last name
+    if (lName.value === '') {
+        error('lastNameErr', 'Please enter your name');
+    }
+    else{
+        var regEx = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
+        if (regEx.test(fName.value) === false) {
+            error('lastNameErr', 'Please enter valid name')
+        }
+        else{
+            error('lastNameErr', '');
+            lastNameErr = false;
         }
     }
     //verify country
@@ -90,29 +109,116 @@ function validator(){
      }
 
         //prevent The form from submiting if there are any errors
-    if ((firstNameErr || emailErr || passwordErr  || countryErr  || acceptionErr) === true) {
+    if ((firstNameErr || emailErr || passwordErr  || countryErr  || acceptionErr || lastNameErr) === true) {
         return false;
     }  
 
 }
 
-//listen for auth status
-auth.onAuthStateChanged(function(user) {
-    console.log(user)
-
-
+const setupUi = (user, snapshot)=>{
     if (user) {
-        console.log(`user loged in ${user}`)
+        greetUser.innerHTML = `Welcome, ${user.email}`
         //if user log in show other btns
         account.style.display = 'block';
         logout.style.display = 'block';
         messageInit.style.display = 'none';
+        accInfo.style.display = 'block';
+        btnSignUp.style.display = 'none';
+        btnSignIn.style.display = 'none';
+
+        db.collection('users').doc(user.uid).get().then(doc =>{
+            var html =
+            `<div>Logged in as:  ${user.email}</div>
+            <div>Name: ${snapshot.data().name}</div>
+            <div>Last Name: ${snapshot.data().surname}</div>
+            
+            <div>Country: ${snapshot.data().country}</div>
+            `
+
+            document.querySelector('.details').innerHTML = html;
+        });       
+        //show info about user in modal Account        
     }
     else{
-        console.log(`user logged out ${user}`)
+        //remove greetings to user
+        greetUser.innerHTML = ``
         account.style.display = 'none';
         logout.style.display = 'none';
         messageInit.style.display = 'block';
+        messageInit.innerHTML = `Sign Up or Login to see account information`;
+        accInfo.style.display = 'none';
+        btnSignUp.style.display = 'block';
+        btnSignIn.style.display = 'block';
+
+        //hide info about account
+    }
+}
+
+//update Values in Firestore
+    const btnChangeN = document.querySelector('#changeNameBtn');
+    const btnChangeLN = document.querySelector('#changeLastNameBtn');
+    const btnCnangeCont = document.querySelector('#changeCountryBtn');
+function changeData(user){
+
+    btnChangeN.addEventListener('click', (e)=>{
+        e.preventDefault()
+        const newName = document.querySelector('#changeName').value;
+        
+        return db.collection('users').doc(user.uid).update({
+            name: newName
+        }).then(()=>{
+    
+        }).catch((errorFirebase)=>{
+            alert(errorFirebase.code.message)
+        });
+    })
+
+    btnChangeLN.addEventListener('click', (e)=>{
+        e.preventDefault()
+        const newLasName = document.querySelector('#changeLastName').value;
+        
+        return db.collection('users').doc(user.uid).update({
+            surname: newLasName
+        }).then(()=>{
+    
+        }).catch((errorFirebase)=>{
+            alert(errorFirebase.code.message)
+        });
+    })
+
+    btnCnangeCont.addEventListener('click', (e)=>{
+        e.preventDefault()
+        const newCountry = document.querySelector('#changeCountry').value;
+        
+        return db.collection('users').doc(user.uid).update({
+            country: newCountry
+        }).then(()=>{
+    
+        }).catch((errorFirebase)=>{
+            alert(errorFirebase.code.message)
+        });
+    })
+}
+
+//listen for auth status
+auth.onAuthStateChanged(function(user) {
+    console.log(user)
+    if (user) {
+
+        db.collection('users').doc(user.uid).onSnapshot(snapshot =>{
+            console.log('Current data:', snapshot.data())
+            setupUi(user, snapshot);
+        })
+        //setupUi(user);
+        //see object user
+        console.log(`user loged in ${user}`)
+        //Greet user
+        changeData(user);
+    }
+    else{
+        console.log(`user logged out ${user}`)
+        
+        setupUi(user);
     }
 
   });
@@ -124,8 +230,16 @@ btnSubmit.addEventListener('click', (e)=>{
     const emailValue = email.value;
     const passValue = password.value;
 
-    if ((firstNameErr || emailErr || passwordErr || countryErr || acceptionErr) === false) {
+    if ((firstNameErr || lastNameErr || emailErr || passwordErr || countryErr || acceptionErr) === false) {
         auth.createUserWithEmailAndPassword(emailValue, passValue).then(cred => {
+
+            return db.collection('users').doc(cred.user.uid).set({
+                name: fName.value,
+                surname: lName.value,
+                country: country.value,
+            })
+
+        }).then(()=>{
 
         }).catch(function(errorFire) {
             // Handle Errors here.
@@ -149,8 +263,6 @@ btnSubmit.addEventListener('click', (e)=>{
 });
   // [END authstatelistener]
 
-
-
 // Sign out the user
 
 logout.addEventListener('click', (e)=>{
@@ -160,14 +272,10 @@ logout.addEventListener('click', (e)=>{
 })
 
 
-
-
 //sign In the user
 var login = document.querySelector('#login-btn');
 login.addEventListener('click', e => {
     e.preventDefault();
-
-
     const emailLogin = document.querySelector('.emailLogin').value;
     const passwordLogin = document.querySelector('.passwordLogin').value;
 
@@ -188,7 +296,30 @@ login.addEventListener('click', e => {
       modalSignIn.style.display = "none";
 });
 
+// resetPassword
+forgotPass.addEventListener('click', e =>{
+    e.preventDefault();
 
-
-
-
+    var email = document.querySelector('.emailForgotPass').value;
+    // [START sendpasswordemail]
+    auth.sendPasswordResetEmail(email).then(function() {
+      // Password Reset Email Sent!
+      // [START_EXCLUDE]
+      alert('Password Reset Email Sent!');
+      // [END_EXCLUDE]
+      modalForgotPass.style.display = "none";
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode == 'auth/invalid-email') {
+        alert(errorMessage);
+      } else if (errorCode == 'auth/user-not-found') {
+        alert(errorMessage);
+      }
+      console.log(error);
+      // [END_EXCLUDE]
+    });
+    // [END sendpasswordemail]
+})
--- javaScript/firebasePWI.js
+++ javaScript/firebasePWI.js
@@ -1,20 +1,32 @@
+//navbar btn
 var btnSignUp = document.querySelector('#signUpId');
 var btnSignIn = document.querySelector('#signInId');
-var btnSettings = document.querySelector('#settingsId');
-var btnAccpunt = document.querySelector('#accountId');
+var btnAccount = document.querySelector('#accountId');
 
 
-
+//link btn in forms
 var btnLogin = document.querySelector(".login");
 var btnForgotPass = document.querySelector(".forgotPassword");
 
 
-
+//close span
 var closeSpan = document.querySelectorAll('.close');
 
+//modals
 var modalSignUp = document.querySelector('.modal-sign-up');
 var modalSignIn = document.querySelector('.modal-sign-in');
+var modalAcc = document.querySelector('.modal-account');
+var modalForgotPass = document.querySelector('.modal-resetPass');
 
+
+btnForgotPass.addEventListener('click', ()=>{
+    modalForgotPass.style.display = 'block';
+    modalSignIn.style.display = 'none';
+})
+
+// btnAccount.addEventListener('click', ()=>{
+//     modalAcc.style.display = 'block';
+// })
 
 
 btnLogin.addEventListener('click', ()=>{
@@ -25,11 +37,13 @@ btnLogin.addEventListener('click', ()=>{
 
 btnSignUp.addEventListener('click', ()=>{
     modalSignUp.style.display = 'block';
+    modalSignIn.style.display = 'none';
 });
 
 
 btnSignIn.addEventListener('click', ()=>{
     modalSignIn.style.display = 'block';
+    modalSignUp.style.display = 'none';
 })
 
 closeSpan[0].addEventListener('click', ()=>{
@@ -39,6 +53,15 @@ closeSpan[0].addEventListener('click', ()=>{
 closeSpan[1].addEventListener('click', ()=>{
     modalSignIn.style.display = "none";
 });
+
+closeSpan[2].addEventListener('click', ()=>{
+    modalAcc.style.display = "none";
+});
+closeSpan[3].addEventListener('click', ()=>{
+    modalForgotPass.style.display = "none";
+});
+
+
 
 
 window.onclick = function(event) {
